208e1fb364a320893d8debf032ae673688163469
takasago <takasago@dummy.com>
2019-03-24 14:23:24 +0900

    6.2.2 finished

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,14 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+
+  test "name should present" do
+    @user.name =  "     "
+    assert_not @user.valid?
+  end
+
+  test "email should present" do
+    @user.email =  "     "
+    assert_not @user.valid?
+  end
 end
