36194232fa38ccb41b2ab36f0fc8fc97035d7f1f
takasago <takasago@dummy.com>
2019-05-13 20:10:39 +0900

    10.2.3 finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,11 +47,13 @@ class UsersController < ApplicationController
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
     end
 
+    # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,10 +18,12 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    assert session[:forwarding_url], edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert_nil session[:forwarding_url]
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
